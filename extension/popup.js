5c07cab31ed5b3848ea52540f714027ff80b025b
davidmunozvi <davidmunozvicode@gmail.com>
2023-04-21 20:08:48 +0200

    refactor and add street view input

--- extension/popup.js
+++ extension/popup.js
@@ -1,36 +1,92 @@
 document.addEventListener("DOMContentLoaded", function () {
-	//Llamada a la API
-	fetch("https://run.mocky.io/v3/aadcb90b-4063-49e3-b01c-8e36762fec69")
-		.then((response) => response.json())
-		.then((data) => generateMultiSelector(data.companies));
+  generateCompanySelector();
+  generateStreetViewEvent();
+  applyCookieState("apply_street_view", setStreetViewState);
 });
 
 function generateMultiSelector(companyList) {
-	let multiSelector = document.getElementById("selctor-company");
-	let options = "";
-	companyList.forEach((company) => {
-		options += `<option value="${company}">${company}</option>`;
-	});
-	multiSelector.innerHTML = options;
+  let multiSelector = document.getElementById("selector-company");
+  let options = "";
+  companyList.forEach((company) => {
+    options += `<option value="${company}">${company}</option>`;
+  });
+  multiSelector.innerHTML = options;
+  setCompanyEvent(multiSelector);
+}
 
-	multiSelector.addEventListener("change", function () {
-		var selectedCompany = multiSelector.value;
-		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-			var currentTab = tabs[0];
-			chrome.scripting.executeScript(
-				{
-					target: { tabId: currentTab.id },
-					function: (selectedCompany) => {
-						document.cookie = `company=${selectedCompany}`;
-					},
-					args: [selectedCompany],
-				},
-				(res) => {}
-			);
-		});
+function setCookie(key, value) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currentTab = tabs[0];
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: currentTab.id },
+        function: (key, value) => {
+          document.cookie = `${key}=${value}`;
+        },
+        args: [key, value],
+      },
+      (res) => {}
+    );
+  });
+  reloadPage();
+}
 
-		chrome.tabs.query({ active: true, currentWindow: true }, function () {
-			chrome.tabs.reload();
-		});
-	});
+function deleteCookie(key) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currentTab = tabs[0];
+    chrome.scripting.executeScript({
+      target: { tabId: currentTab.id },
+      function: (key) => {
+        document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+      },
+      args: [key],
+    });
+  });
+  reloadPage();
+}
+
+function applyCookieState(key, callback) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.cookies.get({ url: tabs[0].url, name: key }, function (cookie) {
+      if (cookie) callback(cookie.value);
+    });
+  });
+}
+
+function setStreetViewState(value) {
+  document.getElementById("street-view").checked = Boolean(value);
+}
+function setCompanyState(value) {
+  document.getElementById("selector-company").value = value;
+}
+
+function setCompanyEvent(multiSelector) {
+  multiSelector.addEventListener("change", function () {
+    var selectedCompany = multiSelector.value;
+    setCookie("company", selectedCompany);
+  });
+}
+
+function generateStreetViewEvent() {
+  let streetViewInput = document.getElementById("street-view");
+  streetViewInput.addEventListener("change", function () {
+    if (streetViewInput.checked) {
+      setCookie("apply_street_view", "true");
+      return;
+    }
+    deleteCookie("apply_street_view");
+  });
+}
+
+function reloadPage() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function () {
+    chrome.tabs.reload();
+  });
+}
+
+function generateCompanySelector() {
+  fetch("https://run.mocky.io/v3/aadcb90b-4063-49e3-b01c-8e36762fec69")
+    .then((response) => response.json())
+    .then((data) => generateMultiSelector(data.companies))
+    .then(() => applyCookieState("company", setCompanyState));
 }
